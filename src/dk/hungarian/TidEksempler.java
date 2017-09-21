d25e3c1f0dddb83a22becb3d22aa80fbf2194e27
Andras Acs <acsandras@gmail.com>
2017-09-21 14:41:15 +0200

    Nu med et eksempel på tid i Java

---
+++ src/dk/hungarian/TidEksempler.java
@@ -0,0 +1,32 @@
+package dk.hungarian;
+
+public class TidEksempler {
+    public static void main(String[] args) {
+        // Obtain the total milliseconds since midnight, Jan 1, 1970
+        long totalMilliseconds = System.currentTimeMillis();
+        System.out.println("Milisekunder siden UNIX er født " + totalMilliseconds);
+
+        // Obtain the total seconds since midnight, Jan 1, 1970
+        long totalSeconds = totalMilliseconds / 1000;
+        System.out.println("Sekunder siden UNIX er født " + totalSeconds);
+
+        // Compute the current second in the minute in the hour
+        long currentSecond = totalSeconds % 60;
+
+        // Obtain the total minutes
+        long totalMinutes = totalSeconds / 60;
+
+        // Compute the current minute in the hour
+        long currentMinute = totalMinutes % 60;
+
+        // Obtain the total hours
+        long totalHours = totalMinutes / 60;
+
+        // Compute the current hour
+        long currentHour = totalHours % 24 + 2;
+
+        // Display results
+        System.out.println("Current time is " + currentHour + ":"
+                + currentMinute + ":" + currentSecond + " GMT+2");
+    }
+}
